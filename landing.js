4d76d172d6662cd4945aa895a1c5c555f710b7cb
Vilmar Jr Ammasi <ammasivilmar2@gmail.com>
2026-05-04 17:06:29 +0800

    Refactor mobile navigation toggle and related features

--- landing.js
+++ landing.js
@@ -1,3 +1,28 @@
+
+// MENU TOGGLE
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+  });
+});
+
+
+
+
+
+
+
+
+
+
 // Mobile Navigation Toggle
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
